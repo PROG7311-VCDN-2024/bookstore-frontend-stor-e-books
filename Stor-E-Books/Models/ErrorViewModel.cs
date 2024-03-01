16a708e132eaacc0aff7c333fc76bc5b49f5e980
ST10216772 <ST10216772@vcconnect.edu.za>
2024-03-01 15:14:29 +0200

    Add project files.

---
+++ Stor-E-Books/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stor_E_Books.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
